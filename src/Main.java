2b172415e3783982dc031c6cb417dd284ef85efe
kittenmasque <volaju@gmail.com>
2023-01-25 20:08:16 -0800

    Created a withdraw fee exclusive to the SavingsAccount subclass

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,34 @@
 import accounts.Account;
 import accounts.CheckingAccount;
+import accounts.SavingsAccount;
+
 public class Main {
     public static void main(String[] args) {
 
         CheckingAccount account = new CheckingAccount(100, .10, 700);
+//Demonstrating the withdraw fee from our Savings account vs Checking account
+        CheckingAccount ca = new CheckingAccount(100, .2, 500);
+        SavingsAccount sa = new SavingsAccount(100, .2);
+
+
+        ca.status();
+        sa.status();
+
+        ca.withdraw(15.00);
+        sa.withdraw(15.00);
+
+
+        ca.status();
+        sa.status();
+
+//Demonstrating the reward points program in our Checking account subclass
         if (account.purchase(10.00)){
             System.out.println("Purchase successful");
         }
         else{
             System.out.println("Purchase failed");
         };
-        System.out.println(account.getRewardPoints());
+        System.out.println("You now have " + account.getRewardPoints() + " Reward Points.");
         account.status();
     }
 }
--- src/accounts/SavingsAccount.java
+++ src/accounts/SavingsAccount.java
@@ -5,4 +5,11 @@ public class SavingsAccount extends Account{
         //super intializes classes in our inheritance heirarchy
         super(balance, interestRate);
     }
+
+    @Override
+    public boolean withdraw(double amount) {
+        double fee = 0.25 * amount;
+        amount += fee;
+        return super.withdraw(amount);
+    }
 }
