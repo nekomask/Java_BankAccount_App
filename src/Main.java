4cbfffb653d9f4a943f7e10255c431075590ea56
kittenmasque <volaju@gmail.com>
2023-01-25 19:05:09 -0800

    Created an Account instance from our Account class that allows us to see how we withdraw and deposit funds into our account; using the status method to show our balance.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
+import accounts.Account;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Account account = new Account(100, .02);
+        account.status();
+        account.withdraw(25.64);
+        account.status();
+        account.deposit(12.50);
+        account.status();
+        account.withdraw(85.46);
+        account.status();
+        account.withdraw(2.99);
+        account.status();
     }
 }
--- src/accounts/Account.java
+++ src/accounts/Account.java
@@ -4,19 +4,38 @@ public class Account {
     private double balance;
     private double interestRate;
 
+    //This is our constructor
     public Account(double balance, double interestRate) {
         this.balance = balance;
         this.interestRate = interestRate;
     }
-
+//method for withdrawing money, takes one parameter- the amount of money to withdraw
     public boolean withdraw(double amount) {
-    if(amount >balance)
-
-    {
+        //if amount to withdraw is greater than our account balance then returns false
+    if(amount >balance) {
+        System.out.println("Sorry, you don't have enough funds-- you will overdraft your account.");
         return false;
     }
-
+//if doesn't return false, the amount is deducted from our balance
     balance -=amount;
     return true;
 }
+//method for depositing money; taking the parameter for the amount of money to deposit
+        public void deposit (double amount) {
+        balance += amount;
+        }
+
+//Getters for our balance and interest rate
+    public double getBalance() {
+        return balance;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    //status method that prints balance on account
+    public void status(){
+        System.out.printf("Balance: %.2f\n", balance);
+    }
 }
