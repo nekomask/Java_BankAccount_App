deeb1ed5cb775c094d716d5c810c78d88915e16f
kittenmasque <volaju@gmail.com>
2023-02-19 15:03:45 -0800

    Added scanner to get input from user to customize checking and saving account balances using deposit, withdraw, and purchase subclass methods.

--- src/Main.java
+++ src/Main.java
@@ -2,35 +2,59 @@ import accounts.Account;
 import accounts.CheckingAccount;
 import accounts.SavingsAccount;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         CheckingAccount account = new CheckingAccount(100, .10, 700);
 
 
         //Demonstrating the withdraw fee from our Savings account vs Checking account
-        CheckingAccount ca = new CheckingAccount(100, .2, 500);
-        SavingsAccount sa = new SavingsAccount(100, .2);
 
+        System.out.print("What is the balance of your Checking account?: $");
+        double checkingBalance = scanner.nextDouble();
+
+        System.out.print("What is the balance of your Savings account?: $");
+        double savingsBalance = scanner.nextDouble();
+
+        System.out.print("What is the interest rate of your Savings account?: ");
+        double savingsInterest = scanner.nextDouble();
+
+        CheckingAccount ca = new CheckingAccount(checkingBalance, 0,0);
+        SavingsAccount sa = new SavingsAccount(savingsBalance,savingsInterest);
 
         ca.status();
         sa.status();
 
-        ca.withdraw(15.00);
-        sa.withdraw(15.00);
+        System.out.print("How much would you like to withdraw from your Checking account?: $");
+        double checkingWithdraw = scanner.nextDouble();
 
-
+        ca.withdraw(checkingWithdraw);
         ca.status();
+
+        System.out.print("How much would you like to withdraw from your Savings account?: $");
+        double savingsWithdraw = scanner.nextDouble();
+        sa.withdraw(savingsWithdraw);
         sa.status();
 
-        ca.deposit(10.51);
-        sa.deposit(10.51);
+        System.out.print("How much would you like to deposit into your Checking account?: $");
+        double checkingDeposit = scanner.nextDouble();
 
+        ca.deposit(checkingDeposit);
         ca.status();
+
+        System.out.print("How much would you like to deposit into your Savings account?: $");
+        double savingsDeposit = scanner.nextDouble();
+        sa.deposit(savingsDeposit);
         sa.status();
 
 //Demonstrating the reward points program in our Checking account subclass
-        if (ca.purchase(10.00)){
+        System.out.print("You are making a credit card purchase and paying it from your checking account. How much is your purchase?: $");
+        double checkingPurchase = scanner.nextDouble();
+
+
+        if (ca.purchase(checkingPurchase)){
             System.out.println("Purchase successful");
         }
         else{
